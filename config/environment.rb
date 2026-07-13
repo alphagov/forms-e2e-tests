0ca35253a47ac4d18e92d852c23803d47f342850
Laurence de Bruxelles <laurence.debruxelles@dsit.gov.uk>
2026-07-13 15:14:39 +0300

    Add settings file for each AWS environment
    Add files with configuration for end to end tests for each environment
    in AWS.
    
    The files are named `aws_#{environment_name}`, with the environment name
    retrieved from `SETTINGS__FORMS_ENV`, which is normally set by
    forms-deploy or the scripts in the bin folder.

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,5 @@ require "config"
 require_relative "initializers/config"
 
 env = ENV["SETTINGS__FORMS_ENV"]
+env = "aws_#{env}" unless env == "local"
 Config.load_and_set_settings(Config.setting_files(__dir__, env))
